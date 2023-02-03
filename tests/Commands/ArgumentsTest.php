2e9ce51fca1a9bfb607c3396327d11ec40c97a79
Artem <armrt@mail.ru>
2023-02-03 11:48:15 +0300

    Тест с использованием провайдера данных

--- tests/Commands/ArgumentsTest.php
+++ tests/Commands/ArgumentsTest.php
@@ -34,4 +34,37 @@ class ArgumentsTest extends TestCase
     // Выполняем действие, приводящее к выбрасыванию исключения
     $arguments->get('some_key');
   }
+
+  // Провайдер данных
+  public function argumentsProvider(): iterable
+  {
+    return [
+      ['some_string', 'some_string'], // Тестовый набор
+      // Первое значение будет передано
+      // в тест первым аргументом,
+      // второе значение будет передано
+      // в тест вторым аргументом
+      [' some_string', 'some_string'], // Тестовый набор No2
+      [' some_string ', 'some_string'],
+      [123, '123'],
+      [12.3, '12.3'],
+    ];
+  }
+
+  // Связываем тест с провайдером данных с помощью аннотации @dataProvider
+  // У теста два агрумента
+  // В одном тестовом наборе из провайдера данных два значения
+  /**
+  * @dataProvider argumentsProvider
+  * @throws ArgumentsException
+  */
+  public function testItConvertsArgumentsToStrings($inputValue, $expectedValue): void 
+  {
+    // Подставляем первое значение из тестового набора
+    $arguments = new Arguments(['some_key' => $inputValue]);
+    $value = $arguments->get('some_key');
+    // Сверяем со вторым значением из тестового набора
+    $this->assertEquals($expectedValue, $value);
+  }
+
 }
