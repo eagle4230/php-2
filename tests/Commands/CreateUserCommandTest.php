a7664ac78dea88edc9a123eb9c677bab6a157477
Artem <armrt@mail.ru>
2023-02-03 18:27:09 +0300

    Настроил тест с помощью анонимной функции

--- tests/Commands/CreateUserCommandTest.php
+++ tests/Commands/CreateUserCommandTest.php
@@ -5,8 +5,13 @@ namespace GB\CP\Blog\Commands;
 use PHPUnit\Framework\TestCase;
 use GB\CP\Blog\Commands\Arguments;
 use GB\CP\Blog\Commands\CreateUserCommand;
+use GB\CP\Blog\Exceptions\ArgumentsException;
 use GB\CP\Blog\Exceptions\CommandException;
+use GB\CP\Blog\Exceptions\UserNotFoundException;
 use GB\CP\Blog\Repositories\UsersRepository\DummyUsersRepository;
+use GB\CP\Blog\Repositories\UsersRepository\UsersRepositoryInterface;
+use GB\CP\Blog\UUID;
+use GB\CP\Blog\User;
 
 class CreateUserCommandTest extends TestCase
 {
@@ -26,4 +31,79 @@ class CreateUserCommandTest extends TestCase
     // Запускаем команду с аргументами
     $command->handle(new Arguments(['username' => 'Ivan']));
   }
+
+  // Тест проверяет, что команда действительно требует имя пользователя
+  public function testItRequiresFirstName(): void
+  {
+    // $usersRepository - это объект анонимного класса,
+    // реализующего контракт UsersRepositoryInterface
+    $usersRepository = new class implements UsersRepositoryInterface
+    {
+      public function save(User $user): void
+      {
+        // Ничего не делаем
+      }
+
+      public function get(UUID $uuid): User
+      {
+        // И здесь ничего не делаем
+        throw new UserNotFoundException("Not found");
+      }
+
+      public function getByUsername(string $username): User
+      {
+        // И здесь ничего не делаем
+        throw new UserNotFoundException("Not found");
+      }
+    };
+
+    // Передаём объект анонимного класса
+    // в качестве реализации UsersRepositoryInterface
+    $command = new CreateUserCommand($usersRepository);
+  
+    // Ожидаем, что будет брошено исключение
+    $this->expectException(ArgumentsException::class);
+    $this->expectExceptionMessage('No such argument: first_name');
+  
+    // Запускаем команду
+    $command->handle(new Arguments(['username' => 'Ivan']));
+  }
+
+  // Функция возвращает объект типа UsersRepositoryInterface
+  private function makeUsersRepository(): UsersRepositoryInterface
+  {
+    return new class implements UsersRepositoryInterface
+    {
+      public function save(User $user): void
+      {
+      }
+
+      public function get(UUID $uuid): User
+      {
+        throw new UserNotFoundException("Not found");
+      }
+  
+      public function getByUsername(string $username): User
+      {
+        throw new UserNotFoundException("Not found");
+      }
+    };
+  }
+
+  // Тест проверяет, что команда действительно требует фамилию пользователя
+  public function testItRequiresLastName(): void
+  {
+    // Передаём в конструктор команды объект, возвращаемый нашей функцией
+    $command = new CreateUserCommand($this->makeUsersRepository());
+    $this->expectException(ArgumentsException::class);
+    $this->expectExceptionMessage('No such argument: last_name');
+  
+    $command->handle(new Arguments([
+      'username' => 'Ivan',
+      // Нам нужно передать имя пользователя,
+      // чтобы дойти до проверки наличия фамилии
+      'first_name' => 'Ivan',
+    ]));
+  }
+
 }
