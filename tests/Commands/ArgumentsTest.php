681963bf154b750a85fe4a4e5c4e3e238289865f
Artem <armrt@mail.ru>
2023-02-03 11:36:45 +0300

    Тест исключений

--- tests/Commands/ArgumentsTest.php
+++ tests/Commands/ArgumentsTest.php
@@ -4,6 +4,7 @@ namespace GB\CP\Blog\UnitTests\Commands;
 
 use PHPUnit\Framework\TestCase;
 use GB\CP\Blog\Commands\Arguments;
+use GB\CP\Blog\Exceptions\ArgumentsException;
 
 class ArgumentsTest extends TestCase
 {
@@ -18,6 +19,19 @@ class ArgumentsTest extends TestCase
     // Проверка
     $this->assertEquals('some_value',$value);
     $this->assertIsString($value);
+  }
 
+  public function testItThrowsAnExceptionWhenArgumentIsAbsent(): void
+  {
+    // Подготавливаем объект с пустым набором данных
+    $arguments = new Arguments([]);
+
+    // Описываем тип ожидаемого исключения
+    $this->expectException(ArgumentsException::class);
+    // и его сообщение
+    $this->expectExceptionMessage("No such argument: some_key");
+
+    // Выполняем действие, приводящее к выбрасыванию исключения
+    $arguments->get('some_key');
   }
 }
