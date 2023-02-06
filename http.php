3e37161cbf53514a116b0e5e7ec04514ad242716
Artem <armrt@mail.ru>
2023-02-06 17:19:39 +0300

    Создал класс Response

--- http.php
+++ http.php
@@ -1,10 +1,22 @@
 <?php
 
 use GB\CP\Http\Request;
+use GB\CP\Http\SuccessfulResponse;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
 // Создаём объект запроса из суперглобальных переменных
 $request = new Request($_GET, $_SERVER);
 
-$path = $request->path();
+//$parameter = $request->query('some_parameter');
+//$header = $request->header('Some-Header');
+//$path = $request->path();
+
+// Создаём объект ответа
+$response = new SuccessfulResponse([
+    'name' => 'Alex',
+    'age' => '43',
+]);
+
+// Отправляем ответ
+$response->send();
