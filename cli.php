18b12365c5643bacc2a5e58a8bab7d5ce4360c65
Artem <armrt@mail.ru>
2023-02-12 16:33:49 +0300

    Настроил логирование исключений в файл

--- cli.php
+++ cli.php
@@ -1,15 +1,20 @@
 <?php
 
 use GB\CP\Blog\{Commands\Arguments, Commands\CreateUserCommand, Exceptions\AppException, User, Post, Comment, UUID};
+use Psr\Log\LoggerInterface;
 
 // Подключаем файл bootstrap.php
 // и получаем настроенный контейнер
 $container = require __DIR__ . '/bootstrap.php';
+
+// Получаем Logger
+$logger = $container->get(LoggerInterface::class);
 
 try {
     // При помощи контейнера создаём команду
     $command = $container->get(CreateUserCommand::class);
     $command->handle(Arguments::fromArgv($argv));
 } catch (AppException $e) {
+    $logger->error($e->getMessage(), ['exception' => $e]);
     echo "{$e->getMessage()}\n";
 }
