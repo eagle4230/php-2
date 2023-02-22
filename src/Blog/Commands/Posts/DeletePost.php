2c98e54bd1ac5006d827538021d80cc8331a45b9
Artem <armrt@mail.ru>
2023-02-23 01:13:44 +0300

    Реализовал обработку флагов к команде

--- src/Blog/Commands/Posts/DeletePost.php
+++ src/Blog/Commands/Posts/DeletePost.php
@@ -2,11 +2,13 @@
 
 namespace GB\CP\Blog\Commands\Posts;
 
+use GB\CP\Blog\Exceptions\PostNotFoundException;
 use GB\CP\Blog\Repositories\PostsRepository\PostsRepositoryInterface;
 use GB\CP\Blog\UUID;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\ConfirmationQuestion;
 
@@ -19,7 +21,6 @@ class DeletePost extends Command
         parent::__construct();
     }
 
-    // Конфигурируем команду
     protected function configure(): void
     {
         $this
@@ -29,6 +30,18 @@ class DeletePost extends Command
                 'uuid',
                 InputArgument::REQUIRED,
                 'UUID of a post to delete'
+            )
+
+            // Добавили опцию
+            ->addOption(
+                // Имя опции
+                'check-existence',
+                // Сокращённое имя
+                'c',
+                // Опция не имеет значения
+                InputOption::VALUE_NONE,
+                // Описание
+                'Check if post actually exists',
             );
     }
 
@@ -54,6 +67,18 @@ class DeletePost extends Command
 
         // Получаем UUID статьи
         $uuid = new UUID($input->getArgument('uuid'));
+
+        // Если опция проверки существования статьи установлена
+        if ($input->getOption('check-existence')) {
+            try {
+                // Пытаемся получить статью
+                $this->postsRepository->get($uuid);
+            } catch (PostNotFoundException $e) {
+                // Выходим, если статья не найдена
+                $output->writeln($e->getMessage());
+                return Command::FAILURE;
+            }
+        }
 
         // Удаляем статью из репозитория
         $this->postsRepository->delete($uuid);
